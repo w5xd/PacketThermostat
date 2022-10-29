6cf4f3b6bacb43279288bbdd5d24b7ea20614ec5
Wayne (Sandpoint) <w5xd@writelog.com>
2022-10-29 11:29:36 -0700

    format

--- PacketThermostat/PacketThermostat.ino
+++ PacketThermostat/PacketThermostat.ino
@@ -600,19 +600,23 @@ namespace
         *p++ = ' ';
         p = formatTemperature(p, degreesCx10FromC7089ADC(tOutsideCx10), 's');
         *p++ = ' ';
-
+        int16_t t; int16_t a;
+        if (hvac->GetTargetAndActual(t, a))
+        {
+            p = formatTemperature(p, t, 't');
+            *p++ = ' ';
+            p = formatTemperature(p, a, 'a');
+            *p++ = ' ';
+        }
+        else 
+        {
+            strcpy(p, "0 0 ");
+            p += 4;
+        }
         rtc.updateTime();
         auto q = rtc.stringTime8601();
         while (*p++ = *q++);
 
-        int16_t t; int16_t a;
-        if (hvac->GetTargetAndActual(t, a))
-        {
-            *p++ = ' ';
-            p = formatTemperature(p, t, 't');
-            *p++ = ' ';
-            p = formatTemperature(p, a, 'a');
-        }
         if (radioSetupOK)
             radio.sendWithRetry(GATEWAY_NODEID, reportbuf, strlen(reportbuf));
 #if USE_SERIAL >= SERIAL_PORT_VERBOSE
